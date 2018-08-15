5b54b73267303c521e75fdb4f3fe48cd8516dd9a
Denis <40804851+onemore8762@users.noreply.github.com>
2018-08-15 11:59:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const YTDL = require("ytdl-core");
 const bot = new Discord.Client();
-const TOKEN = "NDc3MTU1Nzk1MjI2NTkxMjUz.DlVhaw.auZSSBE1okMCVDFoTKfqbQO7BM4"
+
 const PREFIX = "-"
 
 function generateHex(){
